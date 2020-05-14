cc65ba802830fe480f398af22faba91449240ea3
unknown <g.gad@nu.edu.eg>
2020-05-14 16:57:17 +0200

    commit

--- Transformer.py
+++ Transformer.py
@@ -4,53 +4,50 @@ class Transformer ():
 
     def __init__(self, material):
         
-        self.B_peak = None 
-        self.k_c = None 
-        self.alpha = None 
-        self.beta = None 
-
-        self.values = dict() 
-        self.values['frequency'] = None 
-        self.values['N1'] = None 
-        self.values['N2'] = None 
-        self.values['volume'] = None 
-        
         self.option_materials = ['silicon iron', 'powdered iron']
+        self.params = ['B_peak', 'k_c', 'alpha', 'beta', 'frequency', 'N1', 'N2', 'volume', 'lamination_thickness']
+        
+        self.values = dict() 
+        for param in self.params: self.values[param] = None 
+        
         assert material in self.option_materials , 'material chosen is not available'
         self.material = material
 
         self.fill_params()
 
     def fill_params(self):
-        
+        self.values['frequency'] = 50
+        self.values['N1'] = 10
+        self.values['N2'] = 100
+        self.values['volume'] = 0.3  
+        self.values['lamination_thickness'] = 0.2
+
         if self.material == 'silicon iron':
-            self.B_peak = 1.56 
-            self.k_c = 3.388
-            self.alpha = 1.7 
-            self.beta = 1.9
+            self.values['B_peak'] = 1.56 
+            self.values['k_c'] = 3.388
+            self.values['alpha'] = 1.7 
+            self.values['beta'] = 1.9
         elif self.material == 'powdered iron':
-            self.B_peak = 1
-            self.k_c = 1798 
-            self.alpha = 1.02 
-            self.beta = 1.89 
+            self.values['B_peak'] = 1
+            self.values['k_c'] = 1798 
+            self.values['alpha'] = 1.02 
+            self.values['beta'] = 1.89 
     
     def set_value(self, name, value):
-
         assert name in self.values, 'key doesnot exist in self.values'
         self.values[name] = value 
      
     def get_hysterisis_loss(self):
         for key, value in self.values.items():
             assert value, 'cannot calculate hysterisis loss because {} is not defined'.format(key) 
-        return self.k_c * (self.values['frequency']**self.alpha) * (self.B_peak**self.beta) * self.values['volume'] 
+        return self.values['k_c'] * (self.values['frequency']**self.values['alpha']) * (self.values['B_peak']**self.values['beta']) * self.values['volume'] 
 
-
+    def get_eddy_loss(self):
+        for key, value in self.values.items():
+            assert value, 'cannot calculate eddy loss because {} is not defined'.format(key) 
+        return self.values['k_c'] * (self.values['B_peak']**2) * (self.values['frequency']**2) * (self.values['lamination_thickness']**2) * self.values['volume']
 
 t = Transformer('silicon iron') 
 
-t.set_value('frequency', 50) 
-t.set_value('N1', 10) 
-t.set_value('N2', 100) 
-t.set_value('volume', 0.3) 
-
 print(t.get_hysterisis_loss()) 
+print(t.get_eddy_loss()) 
--- main.py
+++ main.py
@@ -16,6 +16,11 @@ class Window(QWidget):
         self.buttons = dict() 
         self.labels = dict() 
 
+        self.transformer_probabilities_widgets = dict() 
+        self.efficiency_widgets = dict() 
+        self.short_circuit_widgets = dict() 
+        
+
         self.setfont() 
         self.init_UI()
 
@@ -27,19 +32,56 @@ class Window(QWidget):
 
     def init_UI(self):
         self.setGeometry(300,300, 1200,600)
-        
-
 
         self.init_tabs() 
+        self.create_widgets_and_hide() 
+
         self.display_image('me.jpg') 
+
         self.show()
+
+    def create_widgets_and_hide(self):
+        self.transformer_probabilities_widgets['materials_window'] = QtGui.QComboBox(self)
+        self.transformer_probabilities_widgets['meterials_window'].addItem("silicon iron")
+        self.transformer_probabilities_widgets['meterials_window'].addItem("powder iron")
+        self.transformer_probabilities_widgets['meterials_window'].move(50, 250)
+        self.transformer_probabilities_widgets['meterials_window'].activated[str].connect(self.material_choice) 
+        self.transformer_probabilities_widgets['meterials_window'].setVisible(False)  
 
 
     def init_tabs(self):
-        self.create_button('Transformer Probabilities', (25, 25, 200, 40), self.on_clicked)
-        self.create_button('Effeciency', (250, 25, 100, 40), self.on_clicked)
-        self.create_button('Short-Circuit Test', (360, 25, 200, 40), self.on_clicked)
+        self.create_button('Transformer Probabilities', (25, 25, 200, 40), self.init_transformer_probability_tab)
+        self.create_button('Effeciency', (250, 25, 100, 40), self.init_efficiency_tab)
+        self.create_button('Short-Circuit Test', (360, 25, 200, 40), self.init_short_circuit_tab) 
         self.create_button('Open-Circuit Test', (570, 25, 200, 40), self.on_clicked)
+
+    def init_transformer_probability_tab(self) :
+        self.hide_all_tabs()
+        for key, value in self.transformer_probabilities_widgets.items() :
+            value.setVisible(True) 
+    
+    def init_efficiency_tab(self):
+        self.hide_all_tabs() 
+        for key, value in self.efficiency_widgets.items():
+            value.setVisible(True) 
+    
+    def init_short_circuit_tab(self):
+        self.hide_all_tabs() 
+        for key, value in self.short_circuit_widgets.items():
+            value.setVisible(True) 
+
+
+    def hide_all_tabs(self):
+        for key, value in self.transformer_probabilities_widgets.items() :
+            value.setVisible(False) 
+        for key, value in self.efficiency_widgets.items():
+            value.setVisible(False) 
+        for key, value in self.short_circuit_widgets.items():
+            value.setVisible(False) 
+
+
+    def material_choice(self, choice) :
+        print("chose:", choice) 
 
     def display_image(self, image_name):
         image = QPixmap(image_name) 
@@ -77,13 +119,22 @@ class Window(QWidget):
         if on_clicked : push_button.clicked.connect(partial(on_clicked, name))  
         return push_button 
 
-    def create_label(self, name, coords):
+    def create_label(self, name, coords, on_clicked):
         label = QtWidgets.QLabel(name, self) 
         label.setGeometry(QtCore.QRect(coords[0], coords[1], coords[2], coords[3]))
         label.setMinimumSize(QtCore.QSize(60, 0))
         self.labels[name] = label 
         
         return label 
+
+    def create_drop_menu(self, options, coords) :
+        menu = QtGui.QComboBox(self)
+        for option in options :
+            menu.addItem(option)
+        menu.move(coords) 
+        menu.activated[str].connect(on_clicked) 
+        return menu 
+
 
     def on_clicked(self, name):
         if self.labels['image_label'].isVisible() : 
